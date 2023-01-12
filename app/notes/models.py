d53494f10f9eaeb34e90bb3ff5dec0e639918312
daniel rios <misterrios@gmail.com>
2023-01-12 10:04:11 +0100

    feat: better many to many relation for admin view

--- app/notes/models.py
+++ app/notes/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
+    tags = models.ManyToManyField('Tag', blank=True)
+
     created_date = models.DateTimeField(auto_now_add=True)
     modifield_date = models.DateTimeField(auto_now=True)
 
@@ -13,7 +15,6 @@ class Note(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
-    notes = models.ManyToManyField(Note)
 
     def __str__(self):
         return self.name
