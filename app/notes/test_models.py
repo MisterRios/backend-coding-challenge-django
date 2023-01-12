80710f561c591f9bc80dfe2e9516698e6fa68a84
daniel rios <misterrios@gmail.com>
2023-01-12 10:37:41 +0100

    test: fix model Notes test

--- app/notes/test_models.py
+++ app/notes/test_models.py
@@ -39,12 +39,10 @@ def test_modify_note(db):
     notes = Note.objects.all()
     assert len(notes) == 1
 
-    note = Note.objects.first()
+    note = Note.objects.get()
 
     assert note.title == "note_title"
     assert note.body == "note_body"
-
-    note = Note.objects.get()
 
     note.title = "new_title"
     note.body = "new_body"
