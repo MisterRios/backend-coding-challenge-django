ea983da547c004234121b8477db1f0803d8eec11
daniel rios <misterrios@gmail.com>
2023-01-12 13:10:32 +0100

    feat: add user serializer

--- app/notes/test_api.py
+++ app/notes/test_api.py
@@ -10,21 +10,24 @@ def get_note_url(id):
     return reverse("note", kwargs={"pk": id})
 
 
-def test_api_list_notes(db):
+def test_api_list_notes(db, django_user_model):
     # TODO: use fixtures to create test client instead of
     # instantiating over and over
     test_client = APIClient()
+
+    owner = django_user_model.objects.create(username="someone", password="something")
+
     # TODO: use factory_boy to create multiple test objects for easier testing
     Tag.objects.create(name="name")
     tag1 = Tag.objects.get(id=1)
     Tag.objects.create(name="name2")
     tag2 = Tag.objects.get(id=2)
 
-    Note.objects.create(title="note_title", body="note_body")
+    Note.objects.create(title="note_title", body="note_body", owner=owner)
     note_1 = Note.objects.get(id=1)
     note_1.tags.add(tag1)
     note_1.save()
-    Note.objects.create(title="note_title2", body="note_body2")
+    Note.objects.create(title="note_title2", body="note_body2", owner=owner)
     note_2 = Note.objects.get(id=2)
     note_2.tags.add(tag2)
     note_2.save()
@@ -37,12 +40,14 @@ def test_api_list_notes(db):
             'title': 'note_title',
             'body': 'note_body',
             'tags': [{'id': 1, 'name': 'name'}],
+            'owner': {'id': 1, 'username': 'someone'},
         },
         {
             'id': 2,
             'title': 'note_title2',
             'body': 'note_body2',
             'tags': [{'id': 2, 'name': 'name2'}],
+            'owner': {'id': 1, 'username': 'someone'},
         },
     ]
 
