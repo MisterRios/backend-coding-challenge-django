cbce048cf3e94ab8199ef852a8b5dae9fc1f2b0c
daniel rios <misterrios@gmail.com>
2023-01-12 12:26:02 +0100

    fix: add url router names to use reverse as reference

--- app/notes/urls.py
+++ app/notes/urls.py
@@ -4,9 +4,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import NoteDetail, NoteList, TagList
 
 urlpatterns = [
-    path('notes/', NoteList.as_view()),
-    path('note/<int:pk>', NoteDetail.as_view()),
-    path('tags/', TagList.as_view()),
+    path('notes/', NoteList.as_view(), name="notes"),
+    path('note/<int:pk>', NoteDetail.as_view(), name="note"),
+    path('tags/', TagList.as_view(), name="tags"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
